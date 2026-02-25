4ec9bc58bd5e78db7151c7de32e5a76b860f4a31
OfeliajTomala <ofeliajtomalat@gmail.com>
2026-02-25 17:37:26 -0500

    Cambios finales en diseño visual de EditProductScreen.kt

--- app/src/main/java/app/compose/appoxxo/ui/screens/EditProductScreen.kt
+++ app/src/main/java/app/compose/appoxxo/ui/screens/EditProductScreen.kt
@@ -4,6 +4,7 @@ import android.net.Uri
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.material3.ExposedDropdownMenuAnchorType
@@ -13,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import app.compose.appoxxo.R
@@ -169,6 +171,7 @@ fun EditProductScreen(
                 label         = "Stock",
                 isError       = stock.isNotEmpty() && stock.toIntOrNull() == null,
                 errorMessage  = "Ingresa un número entero",
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
                 leadingIcon = {
                     Icon(
                         painter = painterResource(R.drawable.ic_inventory),
